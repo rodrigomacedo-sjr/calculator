bf283e5a666706fe31dce26c1cedcb34159380f3
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-04-05 15:29:15 -0300

    feat: implement basic operations

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello, World");
+function add(a, b) {
+  a = Number(a);
+  b = Number(b);
+  return a + b;
+}
+
+function subtract(a, b) {
+  a = Number(a);
+  b = Number(b);
+  return a - b;
+}
+
+function multiply(a, b) {
+  a = Number(a);
+  b = Number(b);
+  return a * b;
+}
+
+function divide(a, b) {
+  a = Number(a);
+  b = Number(b);
+
+  if (!b) {
+    throw new Error(`Invalid dividend ${n}`);
+  }
+  return a / b;
+}
